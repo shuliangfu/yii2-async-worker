770f547cb095aa20265c1eafd1dfce9e7484c50b
shuliangfu <admin@shuliangfu.com>
2016-12-28 16:45:12 +0800

    修改命名空间

--- hook.sh
+++
@@ -1,1 +0,0 @@
-curl -XPOST -H'content-type:application/json' 'https://packagist.org/api/update-package?username=shuliangfu&apiToken=iLaAxMrZc7SUF_eHqOWz' -d'{"repository":{"url":"https://github.com/shuliangfu/yii2-async-worker"}}'
